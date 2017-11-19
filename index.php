f3dc5f4fb73652958dbd7e27194ef800cc8bbad6
alden89 <albertocolman81@gmail.com>
2017-11-19 17:38:00 -0300

    quite la palabra logo de la esquina

--- index.php
+++ index.php
@@ -34,7 +34,6 @@ if(is_loggedin()!="")
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#myPage">Logo</a>
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav navbar-right">
