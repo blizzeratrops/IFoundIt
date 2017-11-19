387b561534b5b59daeb5052c720f0a9a9aec1d5a
alden89 <albertocolman81@gmail.com>
2017-11-19 20:12:29 -0300

    mejora del algoritmo de busqueda

--- biblioteca/busqueda.php
+++ biblioteca/busqueda.php
@@ -7,12 +7,21 @@ function search($query){
     $conn = conectarBD();
 
     $query = trim($query);
+
+    $words = explode(" ", $query);
+    $query = implode(" & ", $words);
+
     $sql = "SELECT a.anuncio_id as id,a.titulo as titulo,a.fecha_creacion as fecha,a.ciudad as ciudad,
+            a.descripcion as descripcion,a.valor as monto,a.usr as usuario 
+            FROM anuncios a
+            WHERE to_tsvector(titulo || '. ' || descripcion) @@ to_tsquery('$query')
+            ORDER BY a.fecha_creacion DESC;";
+    /*$sql = "SELECT a.anuncio_id as id,a.titulo as titulo,a.fecha_creacion as fecha,a.ciudad as ciudad,
             a.descripcion as descripcion,a.valor as monto,a.usr as usuario
             FROM anuncios a
             WHERE a.titulo like '%$query%'
             OR a.descripcion like '%$query%'
-            ORDER BY a.fecha_creacion DESC;";
+            ORDER BY a.fecha_creacion DESC;";*/
     $stmt = runQuery($conn, $sql);
     $stmt->execute();
     //$row=$stmt->fetch(PDO::FETCH_ASSOC);
